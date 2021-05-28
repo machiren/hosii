994329830fe0d80f2c108416fbebe789cb00e5ba
ren machida <r-machida@c3reve.co.jp>
2021-05-29 02:00:32 +0900

    記事周りのフロントAPIデータ取得

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -1,24 +1,34 @@
 import Head from 'next/head';
-import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router'
+import useAspidaSWR from '@aspida/swr';
+import aspida from '@aspida/node-fetch';
+import api from '~/server/api/$api';
+import Link from 'next/link';
 
-const Article = (_props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+const client = api(aspida());
+const Article = () => {
+  const router = useRouter();
+  const query = router.query as unknown as { id: number };
+  const { data, error } = useAspidaSWR(client.articles._articleId(query.id));
+  if (error) {
+    return <p>記事の取得に失敗しました</p>
+  }
+  if (!data) {
+    return <p>Loading...</p>
+  }
   return (
     <>
       <Head>
         <title>記事</title>
       </Head>
       <p>記事</p>
+      <p>
+        <Link href={`articles/${data.id}`}>
+          <a>{data.title}</a>
+        </Link>
+      </p>
     </>
   );
-};
-
-const getServerSideProps: GetServerSideProps = async (_context: GetServerSidePropsContext) => {
-  const article = { id: 1, title: 'TITLE' };
-  return {
-    props: {
-      article
-    },
-  };
 };
 
 export default Article;
--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,28 +1,35 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { GetStaticProps, GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import useAspidaSWR from '@aspida/swr';
+import aspida from '@aspida/node-fetch';
+import api from '~/server/api/$api';
 
-const Articles = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
+const client = api(aspida());
+const Articles = () => {
+  const { data, error } = useAspidaSWR(client.articles);
+  if (error) {
+    return <p>記事の取得に失敗しました</p>
+  }
+  if (!data) {
+    return <p>Loading...</p>
+  }
   return (
     <>
       <Head>
         <title>あったらいいなと思う「欲しい」一覧</title>
       </Head>
       <p>記事一覧</p>
-      <Link href="/articles/1">
-        <a>Link to article</a>
-      </Link>
+      {(
+        data.map(article => (
+          <p>
+            <Link href={`articles/${article.id}`}>
+              <a>{article.title}</a>
+            </Link>
+          </p>
+        ))
+      )}
     </>
   );
-};
-
-const getStaticProps: GetStaticProps = async (_context: GetStaticPropsContext) => {
-  const articles = [{ id: 1, title: 'TITLE' }, { id: 2, title: 'TITLE' }] as const;
-  return {
-    props: {
-      articles
-    },
-  };
 };
 
 export default Articles;
